6c7603dd870e58635aa4b0893d731dbbd4cb390e
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-09-01 12:04:58 +0530

    Update url_page_opening.ps1

--- url_page_opening.ps1
+++ url_page_opening.ps1
@@ -1,5 +1,5 @@
 Write-Host "Script to open URL's on Chrome Browser" -ForegroundColor Red -BackgroundColor Black
-$website = Get-Content C:\Users\IN22917979\Videos\url.txt
+$website = Get-Content C:\Users\tmuru\Videos\url.txt
 $website
 Start-Sleep 1
 Write-Host "The URL list is below show us" -ForegroundColor Red -BackgroundColor Green
