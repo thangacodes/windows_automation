3b3035ba873f2874554de33c16b3110e5674a685
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-10-25 23:40:39 +0530

    Create regedit_value_change.ps1
    initial commit

---
+++ regedit_value_change.ps1
@@ -0,0 +1,35 @@
+##############################################################################
+# Name: Registry Editor file adding and existing value changes               #
+# Date: 25/10/2022                                                           #
+# Author: Thangadurai.Murugan                                                #
+# Note: Execute this script Run as Administrator in PowerShell               #
+##############################################################################
+
+Write-Host "Script is to Modify registry using PowerShell cmdlet" -f Cyan -BackgroundColor Red
+
+Start-Sleep 3 # Sleep for 3 seconds
+
+Write-Host "Set regedit location" -f Cyan -BackgroundColor Red
+
+Write-Host "Navigating to the registry path first and then does the necessary changes" -ForegroundColor Cyan -BackgroundColor Red
+
+Set-Location -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\"
+
+pwd  # checking the current path after above command executes.
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\' -Name FeatureSettingsOverrideMask -Value 3
+
+sleep 3
+
+Write-Host "Check now on regedit" -f Cyan -BackgroundColor Red
+
+echo "Opening registry editor"
+
+regedit
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\' -Name FeatureSettingsOverride -Value 8
+
+regedit
+
+exit 
+
