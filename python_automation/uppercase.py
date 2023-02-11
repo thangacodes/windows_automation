74dfb2fef70ef15fa593d7099d903b3674bbd541
Murugan <thangadurai.murugan@tesco.com>
2023-02-11 19:32:26 +0530

    string conversation using uppercase

---
+++ python_automation/uppercase.py
@@ -0,0 +1,12 @@
+import time
+a ="Extracting ssh_key from ssm-parameter_store"
+print(a.upper())
+b = "ansible playbook execution starts"
+print(b.upper())
+time.sleep (5)
+c = "CI is my current project which am working on"
+print(c.upper())
+
+z = input("Enter the name that you wanted converted as upper case:")
+print(z)
+print(z.upper())
