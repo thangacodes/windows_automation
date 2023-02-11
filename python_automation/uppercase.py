c0ac716b7b53e5e7dc26673ccaf4c5099f366776
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2023-02-11 20:09:53 +0530

    Update uppercase.py

--- python_automation/uppercase.py
+++ python_automation/uppercase.py
@@ -6,7 +6,3 @@ print(b.upper())
 time.sleep (5)
 c = "CI is my current project which am working on"
 print(c.upper())
-
-z = input("Enter the name that you wanted converted as upper case:")
-print(z)
-print(z.upper())
